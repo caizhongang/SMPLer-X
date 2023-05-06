0b365b5b47a6c8d4159e41b7d02057e68667800a
caizhongang <caizhongang@sensetime.com>
2023-05-06 23:42:40 +0800

    Add dinov2

---
+++ main/dinov2_utils/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dinov2',
+    version='',
+    description='dinov2',
+    author='',
+    author_email='',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
