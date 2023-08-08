cadb0622d7c770f502aa5dc2740d763b4aba6eaf
yinwanqi <yinwanqi@sensetime.com>
2023-08-08 13:56:54 +0800

    Fix bug in decoder registration

--- main/transformer_utils/mmpose/models/utils/transformer.py
+++ main/transformer_utils/mmpose/models/utils/transformer.py
@@ -564,7 +564,7 @@ class DetrTransformerDecoderLayer_grouped(BaseTransformerLayer):
         return query
 
 
-@TRANSFORMER_LAYER_SEQUENCE.register_module()
+@TRANSFORMER_LAYER_SEQUENCE.register_module(froce=True)
 class DeformableDetrTransformerDecoder(TransformerLayerSequence):
     """Implements the decoder in DETR transformer.
     Args:
